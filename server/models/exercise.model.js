d78c9c6d9ab851cb3926fefd08fe14792fc9d464
Mr. Shipley <pshipley@gonzaga.org>
2023-02-02 11:38:06 -0500

    add and get users

--- server/models/exercise.model.js
+++ server/models/exercise.model.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const excersiseSchema = new Schema({
+const exerciseSchema = new Schema({
     username: {type: String, required: true},
     description: {type: String, required: true},
     duration: {type: Number, required: true},
@@ -10,3 +10,7 @@ const excersiseSchema = new Schema({
 },{
     timestamps: true,
 });
+
+const Exercise = mongoose.model('Exercise', exerciseSchema);
+
+module.exports = Exercise;
