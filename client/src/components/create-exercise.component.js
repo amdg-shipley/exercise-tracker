f28fdd429cb6de0f44ca3151acbd81524e534d25
Mr. Shipley <pshipley@gonzaga.org>
2023-02-03 12:18:36 -0500

    components for crud pages

---
+++ client/src/components/create-exercise.component.js
@@ -0,0 +1,62 @@
+import React, {Component} from 'react';
+
+export default class CreateExercise extends Component{
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            username: '',
+            description: '',
+            duration: '',
+            date: new Date(),
+            users: []
+        }
+
+    }
+
+    onChangeUserName(e){
+        this.setState({
+            username: e.target.value
+        })
+    }
+    onChangeDescription(e){
+        this.setState({
+            description: e.target.value
+        })
+    }
+    onChangeDuration(e){
+        this.setState({
+            duration: e.target.value
+        })
+    }
+    onChangeDate(date){
+        this.setState({
+            date: date
+        })
+    }
+
+    onSubmit(e){
+        e.preventDefault();
+
+        const exercise = {
+            username: this.state.username,
+            description: this.state.description,
+            duration: this.state.duration,
+            date: this.state.date
+        }
+
+        console.log(exercise)
+
+        window.location = '/';
+    }
+    
+    render(){
+        return(
+            <div>
+                {console.log("hello world")}
+                <p>You are on the Create Exercise component!</p>
+            </div>
+        )
+    }
+}
---
+++ client/src/components/exercises.component.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class Exercises extends Component{
+    render(){
+        return(
+            <div>
+                <p>You are on the Exercise component!</p>
+            </div>
+        )
+    }
+}
