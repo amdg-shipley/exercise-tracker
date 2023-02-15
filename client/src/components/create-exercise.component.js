2ae6450b14002cc02674fafd65b041dc8c644cce
Mr. Shipley <pshipley@gonzaga.org>
2023-02-15 14:28:43 -0500

    getting exercises for dropdown

--- client/src/components/create-exercise.component.js
+++ client/src/components/create-exercise.component.js
@@ -25,10 +25,16 @@ export default class CreateExercise extends Component{
     }
 
     componentDidMount(){
-        this.setState({
-            users: ['test', 'ship'],
-            username: 'test'
-        })
+        axios.get('http://localhost:5000/users/')
+            .then(response => {
+                if (response.data.length > 0){
+                    this.setState({
+                        users: response.data.map(user => user.username),
+                        username: response.data[0].username
+                    })
+                    console.log(response.data[0].username);
+                }
+            })
     }
 
     onChangeUserName(e){
@@ -64,7 +70,7 @@ export default class CreateExercise extends Component{
 
         console.log(exercise)
 
-        axios.post('http://localhost:5001/exercises/add', exercise)
+        axios.post('http://localhost:5000/exercises/add', exercise)
             .then(res => console.log(res.data));
 
             this.setState({
